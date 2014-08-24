fcc59132f50f520eb1417331f883cee059433582
playif <playif@gmail.com>
2014-08-24 15:48:27 +0800

    0.0.4 start flight

--- web/main.dart
+++ web/main.dart
@@ -2,6 +2,11 @@ import "package:play_phaser/phaser.dart";
 
 import "ui.dart";
 
+main() {
+  Game game = new Game(480, 800, WEBGL, '');
+  game.state.add('tyrian', new Tyrian());
+  game.state.start('tyrian');
+}
 
 abstract class Entity extends GameObject {
 //  num x,y;
@@ -10,7 +15,7 @@ abstract class Entity extends GameObject {
 
 
   num getBarValue(int type);
-  //num getBarMaxValue(int type);
+//num getBarMaxValue(int type);
 
 }
 
@@ -19,76 +24,145 @@ class Bar extends TileSprite {
   int barType;
   num maxValue;
 
-  Bar(Game game, Entity entity, [num width=10, num height=5, int barType=0, num maxValue=10, Object key, Object frame])
-    :super(game, 0, 0, 12, 20, key, frame) {
+  Bar(Game game, Entity entity, [num width = 10, num height = 5, int barType = 0, num maxValue = 10, Object key, Object frame])
+  : super(game, 0, 0, 12, 20, key, frame) {
     this.entity = entity;
-    this.barType=barType;
-    this.maxValue=maxValue;
+    this.barType = barType;
+    this.maxValue = maxValue;
   }
 
 
 }
 
 class Flight extends Sprite implements Entity {
-  num HP=10, MHP=10,team =0;
-  Group<Bullet> group;
-  List<Weapon> weapons=new List<Weapon>();
+  num HP = 3,
+  MHP = 3,
+  team = 0;
+  Group<Bullet> bullets;
 
-  num _direction=0;
-  num get direction=>_direction;
-  set direction (num value){
-    _direction=value;
-    rotation=_direction;
+//  List<Weapon> weapons = new List<Weapon>();
+  FlightType flightType;
+
+  bool isEnemy = true;
+
+  num _direction = 0;
+
+  num nextFireTime = 0;
+
+  num get direction => _direction;
+
+  set direction(num value) {
+    _direction = value;
+    rotation = _direction;
   }
 
-  num getBarValue(int type){
+  num getBarValue(int type) {
     return HP;
   }
 
-  Flight(Game game, group, [Object key, Object frame])
-    :super(game, 0, 0, key, frame) {
-    this.group=group;
-
+  Flight(Game game, bullets, [Object key, Object frame])
+  : super(game, 0, 0, key, frame) {
+    this.bullets = bullets;
+    this.key = key;
   }
 
-  addWeapon(Weapon weapon, [Point shift]){
-    //Weapon weapon =new Weapon(this);
-    if(shift != null){
-      weapon.shift=shift;
+//  addWeapon(Weapon weapon, [Point shift]) {
+//    //Weapon weapon =new Weapon(this);
+//    if (shift != null) {
+//      weapon.shift = shift;
+//    }
+//
+//
+//    weapons.add(weapon);
+//  }
+
+//  fire() {
+//    for (Weapon weapon in weapons) {
+//      weapon.tryFire();
+//    }
+//  }
+
+  update() {
+    if (!this.exists) return;
+    if (game.time.now >= nextFireTime) {
+      nextFireTime = game.time.now + flightType.fireCD;
+
+      flightType.fire(this);
+
     }
-
-
-    weapons.add(weapon);
   }
 
-  fire(){
-    for(Weapon weapon in weapons){
-      weapon.tryFire();
+  resetFlight(num x, num y, FlightType type, [bool isEnemy=false]) {
+    this.reset(x, y);
+    this.loadTexture('tyrian', type.frame);
+    this.isEnemy = isEnemy;
+    this.flightType = type;
+    this.MHP = this.HP = type.MHP;
+    //this._fire=fire;
+    if (this.isEnemy) {
+      this.rotation=Math.PI;
     }
   }
 
 }
 
-class Weapon{
+typedef FireFunc(Flight flight, [target]);
+
+class FlightType {
+  num fireCD = 1000;
+  num MHP = 2;
+  Object frame = "011";
+  Function fire = basicFire;
+
+  static FlightType BasicFlight = new FlightType();
+
+
+  static basicFire(Flight flight, [target]) {
+    Group<Bullet> bullets = flight.bullets;
+    Bullet bullet = flight.bullets.getFirstExists(false);
+    num x = flight.x;
+    num y = flight.y;
+    if (bullet != null) {
+      bullet.resetBullet(x, y, BulletType.basicBullet, flight, target);
+
+    } else {
+      print("Error! not enough bullets in pool.");
+    }
+  }
+}
+
+class BulletType {
+  num damage = 1;
+  Object frame = "bullets/ball";
+  Point velocity = new Point(0, -1000);
+
+  static BulletType basicBullet = new BulletType();
+}
+
+class Weapon {
   Flight flight;
-  Point shift=new Point();
-  num cdTime=1000;
-  num cdCounter=0;
+  Point shift = new Point();
+  num cdTime = 1000;
+  num cdCounter = 0;
 
-  Weapon(Flight flight){
-    this.flight=flight;
+  Weapon(Flight flight) {
+    this.flight = flight;
   }
 
-  tryFire(){
+  tryFire() {
 
   }
 }
 
-class Bullet extends Sprite{
-  Group<Bullet> group;
+class Bullet extends Sprite {
+  //Group<Bullet> group;
+  BulletType bulletType;
+  Flight flight;
+  var target;
 
-  Bullet(Game game, [ Object key, Object frame])
-  :super(game, 0, 0, key, frame) {
+  Bullet(Game game, [Object key, Object frame])
+  : super(game, 0, 0, key, frame) {
+    this.key = key;
     //game.physics.enable(this);
     //this.group=group;
 
@@ -96,18 +170,28 @@ class Bullet extends Sprite{
     //group.addChild(this);
   }
 
+  resetBullet(num x, num y, BulletType bulletType, Flight flight, [target]) {
+    this.reset(x, y);
+
+    this.bulletType = bulletType;
+    this.loadTexture('tyrian', bulletType.frame);
+    this.flight = flight;
+    this.target = target;
+
+    this.body.velocity.copyFrom(bulletType.velocity);
+    if (flight.isEnemy) {
+      this.body.velocity.y *= -1;
+    }
+
+  }
 
 }
 
-main() {
-  Game game = new Game(800, 600, WEBGL, '');
-  game.state.add('tyrian', new Tyrian());
-  game.state.start('tyrian');
-}
 
-class FlightInfo{
-  num x=0,y=0;
-  int FlightType=0;
+class FlightInfo {
+  num x = 0,
+  y = 0;
+  int FlightType = 0;
 
 }
 
@@ -115,7 +199,8 @@ class FlightInfo{
 class Tyrian extends State {
 
   preload() {
-    game.load.spritesheet("sword2", "img/Sword2.png", 192);
+    //game.load.spritesheet("sword2", "img/Sword2.png", 192);
+    game.load.spritesheet('kaboom', 'img/explode.png', 128, 128);
     game.load.atlasJSONHash('tyrian', 'img/tyrian.png', 'img/tyrian.json');
     game.load.spritesheet('ground_1x1', 'img/ground_1x1.png', 32);
     game.load.spritesheet('tmw_desert_spacing', 'img/tmw_desert_spacing.png', 32, 32, -1, 1, 1);
@@ -128,169 +213,190 @@ class Tyrian extends State {
   Group<Flight> enemyFlights;
   Group<Bullet> playerBullets;
   Group<Bullet> enemyBullets;
-
+  Group<Sprite> explosions;
 
   List<FlightInfo> playerInfo;
   List<FlightInfo> levels;
 
-  setupLevel(List<FlightInfo> flightInfos, Group<Flight> flights , [bool invert=false]){
 
-    for(FlightInfo flightInfo in flightInfos){
-      Flight flight= flights.getFirstAlive();
-      flight.reset(flightInfo.x,flightInfo.y);
+  setupLevel(List<FlightInfo> flightInfos, Group<Flight> flights, [bool invert = false]) {
+
+    for (FlightInfo flightInfo in flightInfos) {
+      Flight flight = flights.getFirstAlive();
+      flight.reset(flightInfo.x, flightInfo.y);
       flight.anchor.setTo(0.5);
 
-      if(invert){
-        flight.direction= 1;
+      if (invert) {
+        flight.direction = 1;
+      } else {
+        flight.direction = -1;
       }
-      else{
-        flight.direction=-1;
-      }
-
 
 
     }
   }
 
-  clearLevel(){
-    playerFlights.forEachAlive((Sprite g)=>g.kill());
-    enemyFlights.forEachAlive((Sprite g)=>g.kill());
-    playerBullets.forEachAlive((Sprite g)=>g.kill());
-    enemyBullets.forEachAlive((Sprite g)=>g.kill());
+  clearLevel() {
+    playerFlights.forEachAlive((Sprite g) => g.kill());
+    enemyFlights.forEachAlive((Sprite g) => g.kill());
+    playerBullets.forEachAlive((Sprite g) => g.kill());
+    enemyBullets.forEachAlive((Sprite g) => g.kill());
   }
 
   create() {
 
-    sword2 = game.add.sprite(300, 300, "sword2");
-    sword2.animations.add("hit", [0, 1, 2, 3, 4, 5]);
-    sword2.animations.add("hit2", [6, 7, 8, 9, 10, 11]);
-    sword2.anchor.setTo(0.5);
-    k = game.input.keyboard.addKey(Keyboard.A);
+//    sword2 = game.add.sprite(300, 300, "sword2");
+//    sword2.animations.add("hit", [0, 1, 2, 3, 4, 5]);
+//    sword2.animations.add("hit2", [6, 7, 8, 9, 10, 11]);
+//    sword2.anchor.setTo(0.5);
+//    k = game.input.keyboard.addKey(Keyboard.A);
 
 
     //game.physics.startSystem(Physics.ARCADE);
 
-    game.stage.backgroundColor = 0x182d3b;
+    game.stage.backgroundColor = 0x101010;
 
+    //  An explosion pool
+    explosions = game.add.group();
+    explosions.createMultiple(30, 'kaboom');
+    explosions.forEach(setupInvader);
+    //game.scale.s
 
-    playerFlights=game.add.group();
-    playerBullets=game.add.group();
-    enemyFlights=game.add.group();
-    enemyBullets=game.add.group();
+    playerFlights = game.add.group();
+    playerBullets = game.add.group();
+    enemyFlights = game.add.group();
+    enemyBullets = game.add.group();
 
-    playerFlights.enableBody=true;
-    playerFlights.creator=()=> new Flight(game,playerBullets);
+    playerFlights.enableBody = true;
+    playerFlights.creator = () => new Flight(game, playerBullets, "tyrian")
+      ..anchor.setTo(0.5);
+
     playerFlights.createMultiple(100);
 
-    playerBullets.enableBody=true;
-    playerBullets.creator=()=> new Bullet(game);
+    playerBullets.enableBody = true;
+    playerBullets.creator = () => new Bullet(game, "tyrian")
+      ..anchor.setTo(0.5)
+      ..outOfBoundsKill = true
+      ..checkWorldBounds = true;
     playerBullets.createMultiple(1000);
 
-    enemyFlights.enableBody=true;
-    enemyFlights.creator=()=> new Flight(game,enemyBullets);
+    enemyFlights.enableBody = true;
+    enemyFlights.creator = () => new Flight(game, enemyBullets, "tyrian")
+      ..anchor.setTo(0.5);
+
     enemyFlights.createMultiple(100);
 
-    enemyBullets.enableBody=true;
-    enemyBullets.creator=()=> new Bullet(game);
+    enemyBullets.enableBody = true;
+    enemyBullets.creator = () => new Bullet(game, "tyrian")
+      ..anchor.setTo(0.5)
+      ..outOfBoundsKill = true
+      ..checkWorldBounds = true;
+
     enemyBullets.createMultiple(1000);
 
+    Flight flight = playerFlights.getFirstExists(false);
+    flight.resetFlight(200, 700, FlightType.BasicFlight);
 
-    Panel p = new VerticalPanel(game);
-    p.borderWidth = 2;
-    p.position.set(50);
-
-    TextStyle font = new TextStyle(fill: 'white', font: '24px 微軟正黑體');
-
-    Group getItem() {
-      Group item = game.make.group();
-
-      num randW = game.rnd.integerInRange(10, 100);
-      num randH = game.rnd.integerInRange(10, 100);
-
-      Graphics itemBorder = game.make.graphics();
-      itemBorder.lineStyle(2, 0xff00ff);
-      itemBorder.beginFill(2, 0.4);
-      //itemBorder.drawRect(1, 0, randW, randH);
-      itemBorder.drawRect(1, 0, 32, 32);
-      itemBorder.endFill();
-
-      //      Sprite btn = game.add.sprite(150, 10, '__missing');
-      //      btn.inputEnabled = true;
-      //      btn.input.pixelPerfectClick = true;
-      //      num shift = p.length;
-      //      btn.events.onInputDown.add((sender, pointer) {
-      //        //if (p.checkViewPort(pointer)) {
-      //        print("道具 ${shift}");
-      //        //}
-      //      });
-
-      Text text = game.make.text(100, 25, "item ${p.length}", font);
-      text.anchor.setTo(0.5, 0.5);
-      //      text.inputEnabled=true;
-      //      text.events.onInputDown.add((p, e) {
-      //        print("道具 ${p.length}");
-      //      });
+    Flight enemy = enemyFlights.getFirstExists(false);
+    enemy.resetFlight(200, 200, FlightType.BasicFlight, true);
 
 
-      item.add(itemBorder);
-      //item.add(btn);
-      //item.add(text);
-      //itemBorder.y += 50 * p.length;
-      //item.updateTransform();
-      //item.width=200;
-      return item;
-    }
-
-    p.addItem(getItem());
-    p.addItem(getItem());
-    //    p.addItem(getItem());
-    //    p.addItem(getItem());
-    //    p.addItem(getItem());
-    //    p.addItem(getItem());
-
-    FlowPanel p2 = new FlowPanel(game);
-    p2.width = 300;
-    p2.wrapWidth = 600;
-
-
-    p2.borderWidth = 8;
-    p2.name = "p2";
-
-    for (int i = 0; i < 500; i++) {
-      p2.addItem(getItem());
+//    Panel p = new VerticalPanel(game);
+//    p.borderWidth = 2;
+//    p.position.set(50);
+//
+//    TextStyle font = new TextStyle(fill: 'white', font: '24px 微軟正黑體');
+//
+//    Group getItem() {
+//      Group item = game.make.group();
+//
+//      num randW = game.rnd.integerInRange(10, 100);
+//      num randH = game.rnd.integerInRange(10, 100);
+//
+//      Graphics itemBorder = game.make.graphics();
+//      itemBorder.lineStyle(2, 0xff00ff);
+//      itemBorder.beginFill(2, 0.4);
+//      //itemBorder.drawRect(1, 0, randW, randH);
+//      itemBorder.drawRect(1, 0, 32, 32);
+//      itemBorder.endFill();
+//
+//      //      Sprite btn = game.add.sprite(150, 10, '__missing');
+//      //      btn.inputEnabled = true;
+//      //      btn.input.pixelPerfectClick = true;
+//      //      num shift = p.length;
+//      //      btn.events.onInputDown.add((sender, pointer) {
+//      //        //if (p.checkViewPort(pointer)) {
+//      //        print("道具 ${shift}");
+//      //        //}
+//      //      });
+//
+//      Text text = game.make.text(100, 25, "item ${p.length}", font);
+//      text.anchor.setTo(0.5, 0.5);
+//      //      text.inputEnabled=true;
+//      //      text.events.onInputDown.add((p, e) {
+//      //        print("道具 ${p.length}");
+//      //      });
+//
+//
+//      item.add(itemBorder);
+//      //item.add(btn);
+//      //item.add(text);
+//      //itemBorder.y += 50 * p.length;
+//      //item.updateTransform();
+//      //item.width=200;
+//      return item;
+//    }
+//
+//    p.addItem(getItem());
+//    p.addItem(getItem());
+//    //    p.addItem(getItem());
+//    //    p.addItem(getItem());
+//    //    p.addItem(getItem());
+//    //    p.addItem(getItem());
+//
+//    FlowPanel p2 = new FlowPanel(game);
+//    p2.width = 300;
+//    p2.wrapWidth = 600;
+//
+//
+//    p2.borderWidth = 8;
+//    p2.name = "p2";
+//
+//    for (int i = 0; i < 500; i++) {
 //      p2.addItem(getItem());
-//      p2.addItem(getItem());
-//      p2.addItem(getItem());
-//      p2.addItem(getItem());
-    }
-    p2.addItem(getItem());
-    p2.addItem(getItem());
-    p2.addItem(getItem());
-    p2.addItem(getItem());
-    p2.addItem(getItem());
-    //p2.updateTransform();
-
-
-    //p2.position.set(50);
-    //p.addItem(p2);
-
-    //p.visible=false;
-    Border border = new Border(game, "tmw_desert_spacing");
-    //border.rotation=1;
-    border.anchor.set(1);
-    //game.world.add(border);
-    //border.add(p);
-    //border.setPanel(p);
-    //
-    //    //game.world.add(p2);
-    //
-    //    Sprite btn = game.add.sprite(200, 0, '__missing');
-    //    btn.inputEnabled = true;
-    //    btn.events.onInputOver.add((point, e) {
-    //      p.addItem(getItem());
-    //    });
-    //
-    Header header = new Header(game, border, '', 2);
+    ////      p2.addItem(getItem());
+    ////      p2.addItem(getItem());
+    ////      p2.addItem(getItem());
+    ////      p2.addItem(getItem());
+//    }
+//    p2.addItem(getItem());
+//    p2.addItem(getItem());
+//    p2.addItem(getItem());
+//    p2.addItem(getItem());
+//    p2.addItem(getItem());
+//    //p2.updateTransform();
+//
+//
+//    //p2.position.set(50);
+//    //p.addItem(p2);
+//
+//    //p.visible=false;
+//    Border border = new Border(game, "tmw_desert_spacing");
+//    //border.rotation=1;
+//    border.anchor.set(1);
+//    //game.world.add(border);
+//    //border.add(p);
+//    //border.setPanel(p);
+//    //
+//    //    //game.world.add(p2);
+//    //
+//    //    Sprite btn = game.add.sprite(200, 0, '__missing');
+//    //    btn.inputEnabled = true;
+//    //    btn.events.onInputOver.add((point, e) {
+//    //      p.addItem(getItem());
+//    //    });
+//    //
+//    Header header = new Header(game, border, '', 2);
 
     //game.world.add(header);
     //header.offset += p.position;
@@ -413,17 +519,66 @@ class Tyrian extends State {
     //    cursors = game.input.keyboard.createCursorKeys();
   }
 
+  setupInvader(Sprite invader) {
+    invader.anchor.x = 0.5;
+    invader.anchor.y = 0.5;
+    invader.animations.add('kaboom');
+  }
+
   update() {
-    Point p = game.input.position;
-    //game.input.mousePointer.justReleased()
-    if (game.input.mousePointer.justPressed()) {
-      sword2.position.copyFrom(p);
-      sword2.revive();
-      sword2.play("hit2", 16.66, false, true);
-    }
+    game.physics.arcade.overlap(enemyBullets, playerFlights, collisionHandler);
+    game.physics.arcade.overlap(playerBullets, enemyFlights, collisionHandler);
+
+
+//    Point p = game.input.position;
+//    //game.input.mousePointer.justReleased()
+//    if (game.input.mousePointer.justPressed()) {
+//      sword2.position.copyFrom(p);
+//      sword2.revive();
+//      sword2.play("hit2", 16.66, false, true);
+//    }
     //game.physics.arcade.collideSpriteVsGroup(bot,aliens);
     //bot.rotation += 0.1;
     //bot.x+=1;
+  }
+
+  collisionHandler(Bullet bullet, Flight flight) {
+
+    //  When a bullet hits an alien we kill them both
+    bullet.kill();
+    flight.HP -= bullet.bulletType.damage;
+
+    var explosion = explosions.getFirstExists(false);
+    explosion.reset(bullet.x, bullet.y);
+    explosion.play('kaboom', 30, false, true);
+
+    if (flight.HP <= 0) {
+      flight.kill();
+    }
+
+    //flight.kill();
+
+//  //  Increase the score
+//  score += 20;
+//  scoreText.text = scoreString + score.toString();
+//
+//  //  And create an explosion :)
+//  var explosion = explosions.getFirstExists(false);
+//  explosion.reset(alien.body.x, alien.body.y);
+//  explosion.play('kaboom', 30, false, true);
+//
+//  if (aliens.countLiving() == 0) {
+//    score += 1000;
+//    scoreText.text = scoreString + score.toString();
+//
+//    enemyBullets.forEach((Sprite s) => s.kill());
+//    stateText.text = " You Won, \n Click to restart";
+//    stateText.visible = true;
+//
+//    //the "click to restart" handler
+//    game.input.onTap.addOnce(restart);
+//  }
+
   }
 
   render() {
